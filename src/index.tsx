478f48a6de22332a83e4a0616991352aaefc87e2
dgesteves <diogo.esteves.goncalves@gmail.com>
2022-07-18 11:33:06 +0100

    add log web vitals

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-//import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-//reportWebVitals(console.log);
+
+reportWebVitals(console.log);
