516d060ae92e810ce4770fb0991efea06753e9c8
dgesteves <diogo.esteves.goncalves@gmail.com>
2022-07-18 11:40:22 +0100

    add test constants and expected responses from api

---
+++ src/constants/tests/index.ts
@@ -0,0 +1,25 @@
+export const DEFAULT_IP_ADDRESS = "111.111.111.111";
+export const GETOWNIP_RESPONSE = {
+  ip: DEFAULT_IP_ADDRESS,
+};
+export const GETIPDATA_RESPONSE = {
+  as: {
+    asn: 2516,
+    domain: "http://www.kddi.com/english/index.html",
+    name: "APNIC-AS-X-BLOCK",
+    route: "111.111.0.0/16",
+    type: "NSP",
+  },
+  ip: "111.111.111.111",
+  isp: "KDDI CORPORATION",
+  location: {
+    city: "Uchikanda",
+    country: "JP",
+    geonameId: 11790348,
+    lat: 35.69143,
+    lng: 139.76917,
+    postalCode: "",
+    region: "Tokyo Prefecture",
+    timezone: "+09:00",
+  },
+};
